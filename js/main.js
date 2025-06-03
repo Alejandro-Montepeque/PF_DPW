f830c16eaa29a9890ef2e837fc1f7c5ff6e54ca1
alejandro-montepeque <jonathanmontepeque@gmail.com>
2025-06-03 08:29:42 -0600

    add: new way to call the header

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ // Se hace una petición para obtener el archivo 'navbarComponent.html'y donde se encuentra ubicado
+    fetch('../components/navbarComponent.html')
+        // Cuando la respuesta llega, se convierte a texto (el contenido HTML)
+        .then(response => response.text())
+        // Con el contenido HTML recibido, se inserta dentro del elemento con id 'navbar-container'
+        .then(html => {
+            document.getElementById('navbar-container').innerHTML = html;
+        })
+        //Al haber un error muestra en la consola esto
+        .catch(error => console.error('No se encuentra o no esta disponible el navbar', error))
