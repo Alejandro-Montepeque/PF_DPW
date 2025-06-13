03ce2596ced379d4c651808ab829e06676544b6d
alejandro-montepeque <jonathanmontepeque@gmail.com>
2025-06-12 21:13:10 -0600

    add: validation for the form

--- src/main.js
+++ src/main.js
@@ -14,3 +14,43 @@ fetch('/components/navbarComponent.html')
   })
   //Al haber un error muestra en la consola esto
   .catch(error => console.error('No se encuentra o no esta disponible el navbar', error))
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sendBtn = document.getElementById('sendBtn');
+
+  sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const fields = [
+      { id: 'name', label: 'Nombre' },
+      { id: 'email', label: 'Email' },
+      { id: 'phone', label: 'telefono' },
+      { id: 'message', label: 'Mensaje' },
+    ];
+
+    for (const field of fields) {
+      const value = document.getElementById(field.id).value.trim();
+      if (!value) {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Campo vacío',
+          text: `Por favor, complete el campo "${field.label}".`,
+        });
+        return;
+      }
+    }
+
+    // Mostrar alerta de éxito
+    Swal.fire({
+      icon: 'success',
+      title: 'Datos enviados con éxito',
+      text: 'Espere de 3 a 5 horas para ser contactado.',
+    }).then(() => {
+      // Limpiar los campos después de que el usuario cierre la alerta
+      fields.forEach(field => {
+        document.getElementById(field.id).value = '';
+      });
+    });
+  });
+});
