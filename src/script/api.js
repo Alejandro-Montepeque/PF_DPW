71de6a2242356bc81bfec00004c1e7d3fdefc030
alejandro-montepeque <jonathanmontepeque@gmail.com>
2025-09-29 09:26:42 -0600

    add & remove: add new background, new inputs stylesfor form, add new labels for this view and remove code usless and background

--- src/script/api.js
+++ src/script/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// Crear instancia de Axios con baseURL desde variables de entorno
+// Crea la instancia de Axios con baseURL tomdada desde las variables de entorno
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
